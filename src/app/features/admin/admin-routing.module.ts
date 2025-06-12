51660dbb5ec726763afe34d5b6daa68ec4216fa4
IIPL\1000607 <piyush.srivastava@compunneldigital.com>
2025-06-12 11:22:10 +0530

    login page

--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './../admin/admin.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   {
@@ -26,6 +27,12 @@ const routes: Routes = [
         path: 'users',
         loadChildren: () =>
           import('./components/users/users.module').then(m => m.UsersModule),
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent
+        // loadChildren: () =>
+        //   import('./components/profile/profile.module').then(m => m.ProfileModule),
       },
       {
         path: '',
--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AdminComponent,
+    ProfileComponent,
   ],
   imports: [
     CommonModule,
