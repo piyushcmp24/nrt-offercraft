661a1f45cdd07551e8166ae288df77a8a5129eb4
IIPL\1000607 <piyush.srivastava@compunneldigital.com>
2025-06-12 11:43:25 +0530

    changes in component

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AdminComponent,
-    ProfileComponent,
+    AdminComponent
   ],
   imports: [
     CommonModule,
